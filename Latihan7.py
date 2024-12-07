7d45b6f1885fc8a592f84952f1e7bc02ff6d3ec8
fandi a <fandiiardy@gmail.com>
2024-12-07 17:39:01 +0700

    add latihan 7

---
+++ Latihan7.py
@@ -0,0 +1,13 @@
+# Ketentuan
+# 1. IF nya
+# 2. Kondisinya
+# 3. Aksinya
+
+# if <kondisi>:<aksi>
+# ATAU
+# if <kondisi>:
+#    <aksi>
+
+nama = input("Masukkan Nama Anda : ")
+if nama == "alber":
+    print("Anda adalah alber")
