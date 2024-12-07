af81680f8af415a0ac8bd80f9e0f10060c66a897
fandi a <fandiiardy@gmail.com>
2024-12-07 17:39:38 +0700

    add latihan 9

---
+++ Latihan9.py
@@ -0,0 +1,12 @@
+jabatan = input("Masukkan jabatan Anda : ")
+
+if jabatan == "III/a":
+    print("Anda adalah penata muda")
+elif jabatan == "III/b":
+    print("Anda adalah penata muda Tk.I")
+elif jabatan == "III/c":
+    print("Anda adalah penata")
+elif jabatan == "III/d":
+    print("Anda adalah penata Tk.I")
+else:
+    print("Jabatan yang lainnya")
