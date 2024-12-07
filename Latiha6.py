7308572c38825e2f97ec2c4691adb834cf7878c5
fandi a <fandiiardy@gmail.com>
2024-12-07 17:38:38 +0700

    Add Lat6

---
+++ Latiha6.py
@@ -0,0 +1,11 @@
+x = 3
+y = 5
+
+# Operasi penjumlahan
+Jawaban1 = x**y
+print(x, " ** ", y, " = ", Jawaban1)
+
+# Conba untuk buat operasi aritmatika yang lain (pengurangan (-), perkalian (*), pembagian (/), perpangkatan (**), modulo (%))
+
+Jawaban2 = x % y
+print(x, " % ", y, " = ", Jawaban2)
